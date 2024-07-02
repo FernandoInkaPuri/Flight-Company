a88c3e1f860e3516e6431f9774347cffae86be3a
Fernando Puri <luiz.gomez@rebase.com.br>
2024-07-02 18:08:17 -0300

    start form and study how use params in sinatra

--- Sinatra App/hello.rb
+++ Sinatra App/hello.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
+require 'byebug'
+
+class TicketSearch
+
+end
+
+
 
 get '/' do
-  'Hello world!'
+  page = "
+  <h1> Bem vindos à Hawks linhas aéreas! </h1>
+  <h3> Por favor, preencha com as informações para a busca da passagem </h3>
+  <form>
+    <label> Aeroporto de partida </label><br>
+    <input type='text' id='route_origin' name='route_origin'><br>
+    <label> Aeroporto de Destino </label><br>
+    <input type='text' id='route_destiny' name='route_destiny'><br>
+    <label> Data de partida </label><br>
+    <input type='text' id='departure_date' name='departure_date'><br>
+    <label> Data de retorno </label><br>
+    <input type='text' id='return_date' name='return_date'><br>
+    <input type='submit' value='Enviar'>
+  <form>
+  "
+  byebug
+end
+
+post '/xuxa' do
+  byebug
 end
