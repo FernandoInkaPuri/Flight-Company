834d0438e84798866b04d55cf60507dcfd4a2778
Fernando Puri <luiz.gomez@rebase.com.br>
2024-07-04 14:52:31 -0300

    page lists flights returned by the API, but the page is ugly because it doesnt have CSS

--- Sinatra App/hello.rb
+++ Sinatra App/hello.rb
@@ -15,7 +15,7 @@ class TicketSearch
     header = {'x-rapidapi-key': ENV['RAPID_APIKEY']}
 
     response = Net::HTTP.get(uri, headers = header)
-    byebug
+    # byebug
     show_flights(response)
   end
 
@@ -47,9 +47,9 @@ class TicketSearch
         if itinerary['legs'].size == 1
           flights[:one_way] << "Empresa #{company_name}, horário de partida: #{departure_time}, previsão de chegada: #{arrival_time}, preço: #{price}"
         elsif index == 0
-          flights[:roundtrip][:departure] << "Empresa #{company_name}, horário de partida: #{departure_time}, previsão de chegada: #{arrival_time}"
+          flights[:roundtrip][:departure] << "Empresa #{company_name}, horário de partida: #{departure_time}, previsão de chegada: #{arrival_time} "
         elsif index == 1
-          flights[:roundtrip][:return] << "Retorno: Empresa #{company_name}, horário de partida: #{departure_time}, previsão de chegada: #{arrival_time}, preço total da viagem: #{price}"
+          flights[:roundtrip][:return] << "Empresa #{company_name}, horário de partida: #{departure_time}, previsão de chegada: #{arrival_time}, preço total da viagem: #{price}"
         end
       end
     end
@@ -60,34 +60,11 @@ end
 
 
 get '/' do
-  page = "
-  <h1> Bem vindos à Hawks linhas aéreas! </h1>
-  <h3> Por favor, preencha com as informações para a busca da passagem </h3>
-  <form action='/search' method='post'>
-    <label> Aeroporto de partida </label><br>
-    <input type='text' id='route_origin' name='route_origin'><br>
-    <label> Aeroporto de Destino </label><br>
-    <input type='text' id='route_destiny' name='route_destiny'><br>
-    <label> Data de partida </label><br>
-    <input type='text' id='departure_date' name='departure_date'><br>
-    <label> Data de retorno </label><br>
-    <input type='text' id='return_date' name='return_date'><br>
-    <input type='submit' value='Enviar'>
-  <form>
-  "
-
-  # response = {
-  #   "data": {
-  #       "context": {
-  #           "status": "incomplete",
-  #           "sessionId": "ClQIARJQCk4KJDkzYzFkNGQ4LTM1MTgtNDFmZC1iZmI1LTE3Y2ZkMWJhMzhkMRACGiQ3OThkZWY1My0wN2RmLTQ3YzgtOTc3Ni0xMmFkOTRjODUyMzYSKHVzc185NGY1MjlmNi0wNDkzLTQ0MWYtYTliOC0yMGE1MDM2YTI0NzQ=",
-  #           "totalResults": 10
-  #       }}}
-  # page = "<p><%= #{response} %></p>"
+  erb :index
 end
 
 post '/search' do
   @result = TicketSearch.search(params)
-  byebug
-  page = "<p><%= #{@result} %></p>"
+
+  erb :search
 end
